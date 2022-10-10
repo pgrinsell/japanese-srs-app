77ac52b5efb2628e9e66b7919b9fd2b23ec76b62
Patrick Grinsell <pgrinsell@outlook.com>
2022-10-10 19:59:46 +0100

    Adds Joy UI and other dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
+import { useState, useEffect } from 'react';
+import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
+import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+import TextField from '@mui/joy/TextField';
+import Button from '@mui/joy/Button';
+import Box from '@mui/joy/Box';
+import { toKana, isKana, isRomaji } from 'wanakana';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const theme = extendTheme({
+  components: {
+    JoyTextField: {
+      defaultProps: {
+        color: 'primary',
+        variant: 'soft'
+      }
+    }
+  }
+});
+
+const defaultState = {
+  reviews: []
+};
+
+const App = () => {
+  const [reading, setReading] = useState('');
+  const [meaning, setMeaning] = useState('');
+
+  const handleSubmitLesson = e => {
+    e.preventDefault();
+
+    const review = {
+      meanings: [meaning.toLowerCase()],
+      readings: [reading],
+      //nextReview: Date.now()
+    };
+
+    console.log('review', review);
+
+    //setState({ reviews: [...state.reviews, review] });
+  };
+
+  useEffect(() => {
+    console.log(toKana(reading));
+  }, [reading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssVarsProvider theme={theme}>
+      <Card variant="outlined" sx={{ m: 2 }}>
+        <form onSubmit={handleSubmitLesson}>
+          <Box sx={{ display: "grid", gap: 2 }}>
+            <Typography>Add a new lesson</Typography>
+            <TextField
+              type="text"
+              label="Reading"
+              value={reading}
+              onChange={e => setReading(e.target.value)}
+            />
+            <TextField
+              type="text"
+              label="Meaning"
+              value={meaning}
+              onChange={e => setMeaning(e.target.value)}
+            />
+            <Button type="submit">Add lesson</Button>
+          </Box>
+        </form>
+      </Card>
+      <Card variant="outlined" sx={{ m: 2 }}>
+        <Typography>Reviews</Typography>
+      </Card>
+      <Card variant="outlined" sx={{ m: 2 }}>
+        <Typography>Schedule</Typography>
+      </Card>
+    </CssVarsProvider>
   );
-}
+};
 
 export default App;
